4a9f0204158d0091b96743de1af75f886dbfa442
dznet <kuchin.ea@gmail.com>
2016-11-03 13:59:50 +0200

    rsync my fs

---
+++ README.md
@@ -0,0 +1,6 @@
+-Backup/restore my laptop OS
+#
+CFS  -- current filesystem device
+NFS  -- new filesystem device
+CDIR -- temp directory for current fs
+NDIR -- temp directory for new fs
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+CFS=/dev/sda1
+NFS=/dev/sdb1
+CDIR=/mnt/old
+NDIR=/mnt/new 
+sudo mkdir $CDIR $NDIR
+sudo mount $CFS $CDIR
+sudo mount $NFS $NDIR
+sudo rsync -xav --progress --exclude-from=exclude $CDIR $NDIR
+sudo umount $CFS $NEFS
+sudo rm -r $CDIR $NDIR
